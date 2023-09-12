22831e77270cba3f191702827e033fa38280b73d
jrundht <joerund.tangen@gmail.com>
2023-09-12 19:05:45 +0200

    BinarySearchTree

---
+++ oppgaver/BinarySearchTree.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self, x):
+        self.left = None
+        self.right = None
+        self.element = x
+        
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+    
+    #O(h) where h is the height of the tree
+    #O(n) worst case for n elements. if balanced O(log(n))
+    def Insert(self,v , x): 
+        newNode = Node(x)
+        if self.root == None:
+            self.root = newNode
+        if v == None:
+            v = newNode
+        elif x < v.element:
+            v.left = self.Insert(v.left, x)
+        else:
+            v.right = self.Insert(v.right, x)
+        return v
+    
+    #same big O as Insert
+    def Search(self, v, x): 
+        if v == None:
+            return None
+        elif v.element == x:
+            return v
+        elif x < v.element:
+            return self.Search(v.left, x)
+        elif x > v.element:
+            return self.Search(v.right, x)
+    
+    def FindMin(self, v):
+        if v.left != None:
+            return self.FindMin(v.left)
+        elif v.right != None:
+            return self.FindMin(v.right)
+        else:
+            return v
+    
+    def Remove(self, v, x):
+        if v == None:
+            return None
+        elif x < v.element:
+            v.left = self.Remove(v.left, x)
+            return v
+        elif x > v.element:
+            v.right = self.Remove(v.right, x)
+            return v
+        elif v.left == None:
+            return v.right
+        elif v.right == None:
+            return v.left
+        u = self.FindMin(v.right)
+        v.element = u.element
+        v.right = self.Remove(v.right, u.element)
+        return v
+    
+
+def InRange(B, a, b):
+    v = B.root
+    if a >= v.element: #go right
+        if a == v.element:
+            print(v.element)
+        while v
+    
+        
+B = BinaryTree()
+v = B.Insert(None, 10)
+B.Insert(v, 15)
+B.Insert(v, 12)
+B.Insert(v, 17)
+
+B.Insert(v, 5)
+B.Insert(v, 2)
+B.Insert(v, 7)
