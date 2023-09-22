1aa49ac898d37b869ad84a038e96b2ef42c721e1
jrundht <joerund.tangen@gmail.com>
2023-09-22 16:48:03 +0200

    last modifications to simple and very slow teque with python arrays

--- Oblig1/teque/teque.py
+++ Oblig1/teque/teque.py
@@ -1,115 +1,21 @@
 import time
-# # alternativ for forsøk på raskere kjøretid
-# class Node:
-#     def __init__(self, x):
-#         self.data = x
-#         self.next = None
-#         self.prev = None
-    
-# class Linkedlist:
-#     def __init__(self):
-#         self.head = None
-#         self.tail = None
-#         self.middle = None
-#         self.size = 0
-    
-#     def get(self, pos):
-#         if pos > self.size - 1 or pos < 0:
-#             # print("index out of range")
-#             return "index out of range"
-#         i = 0
-#         node = self.head
-#         while i < pos:
-#             node = node.next
-#             i += 1
-#         return node.data
-    
-#     def updateMiddleAp(self):
-#         if self.size % 2 == 0:
-#             self.middle = self.middle.next
-    
-#     def updateMiddlePre(self):
-#         if self.size % 2 == 1:
-#             self.middle = self.middle.prev
-    
-#     def findMiddle(self):
-#         x = (self.size) // 2
-#         node = self.head
-#         while x > 1:
-#             node = node.next
-#             x -= 1
-#         return node
-        
-#     def append(self, x):
-#         newNode = Node(x)
-#         self.size += 1
-#         if not self.head:
-#             self.head = self.middle = self.tail = newNode
-#             return
-#         self.tail.next = newNode
-#         newNode.prev = self.tail
-#         self.tail = newNode
-#         self.updateMiddleAp()
-    
-#     def prepend(self, x):
-#         newNode = Node(x)
-#         self.size += 1
-#         if not self.head:
-#             self.head = self.middle = self.tail = newNode
-#             return
-#         self.head.prev = newNode
-#         newNode.next = self.head
-#         self.head = newNode
-#         self.updateMiddlePre()
 
-#     def middlepend(self, x):
-#         newNode = Node(x)
-#         if self.size == 0 or self.size == 1:
-#             self.append(x)
-#             return
-
-#         if self.size % 2 == 0: 
-#             newNode.next = self.middle
-#             newNode.prev = self.middle.prev
-#             if self.middle.prev:
-#                 self.middle.prev.next = newNode
-#             else:
-#                 self.head = newNode  
-#             self.middle.prev = newNode
-            
-#             self.middle = newNode
-#         else:  
-#             newNode.next = self.middle.next
-#             newNode.prev = self.middle
-#             if self.middle.next:
-#                 self.middle.next.prev = newNode
-#             else:
-#                 self.tail = newNode 
-#             self.middle.next = newNode
-#         self.size += 1
-
-#     def print_list(self):
-#         curr_node = self.head
-#         while curr_node:
-#             print(curr_node.data, end=' -> ')
-#             curr_node = curr_node.next
-#         print("None")
-
+# O(1)
 def push_back(x):
     A.append(x)
 
+# O(N)
 def push_front(x):
-    # A.prepend(x)
     global A
     A.insert(0, x)
 
+# O(N/2)
 def push_middle(x):
-    # A.middlepend(x)
     global A
-    A.insert((len(A)+1)/2, x)
+    A.insert((len(A)+1)//2, x)
 
+# O(1)
 def get(i):
-    # return A.get(i)
     return A[i]
 
 def print_list():
@@ -117,14 +23,13 @@ def print_list():
         print(i, end=' -> ')
     print("None")
 
-# A = Linkedlist()
 t = time.time_ns()
 filename = input()
 A = []
 file = open(filename, "r")
 N = int(file.readline())
 filename = f"myOutput1_{N}"
-file1 = open(filename, "w")
+file1 = open("Outputs/"+filename, "w")
 while N != 0:
     inp = file.readline()
     lst = inp.split()
@@ -138,8 +43,9 @@ while N != 0:
         push_middle(x)
     else:
         file1.write(f"{A[x]}" + "\n")
-        print(get(x))
+        # print(get(x))
     N -= 1
 file.close()   
 print(f"Time used: {(time.time_ns() - t)/1000}")
+print_list()
 # print_list()
