91125eefa6e85b849c6e563e4edf2e5c0f861f1b
jrundht <joerund.tangen@gmail.com>
2023-10-05 14:29:17 +0200

    Array balancer

---
+++ Oblig2/BalanceArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class BalanceArray{
+    public static void balancer(ArrayList<Integer> array){
+        if(array.size() < 2){
+            System.out.println(array.get(0));
+            return;
+        }
+        if(array.size() < 3){
+            System.out.println(array.get(1));
+            System.out.println(array.get(0));
+            return;
+        }
+        System.out.println(array.get((array.size() / 2)));
+        balancer(new ArrayList<>(array.subList((array.size() / 2) + 1, array.size())));
+        balancer(new ArrayList<>(array.subList(0, (array.size() / 2))));
+    }
+    public static void main(String[] args){
+        ArrayList<Integer> sortedList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        sortedList.add(0);
+        while(scanner.hasNext()){
+            sortedList.add(scanner.nextInt());
+        }
+        balancer(sortedList);
+    }
+}
