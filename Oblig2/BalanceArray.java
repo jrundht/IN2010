67bb4e6e965530f01b798c8c33726dbd59b4183b
jrundht <joerund.tangen@gmail.com>
2023-10-05 17:20:05 +0200

    removed obsolete code

--- Oblig2/BalanceArray.java
+++ Oblig2/BalanceArray.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class BalanceArray{
-    /*
-     * run: seq 20 | java BalanceArray | java BalanceChecker
-     */
     public static void balancer(ArrayList<Integer> array){
         if(array.size() < 2){
             System.out.println(array.get(0));
@@ -19,6 +16,9 @@ public class BalanceArray{
         balancer(new ArrayList<>(array.subList((array.size() / 2) + 1, array.size())));
         balancer(new ArrayList<>(array.subList(0, (array.size() / 2))));
     }
+    /*
+     * run: seq 20 | java BalanceArray | java BalanceChecker
+     */
     public static void main(String[] args){
         ArrayList<Integer> sortedList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
