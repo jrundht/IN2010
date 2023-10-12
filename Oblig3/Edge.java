792d4d1b1eab14f556eb792467db603f2f7bdc6b
jrundht <joerund.tangen@gmail.com>
2023-10-12 16:58:35 +0200

    Tuple representing edge between nodes

--- Oblig3/Edge.java
+++ Oblig3/Edge.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 public class Edge {
-    String node1;
-    String node2;
+    Vertex node1;
+    Vertex node2;
 
-    public Edge(String node1, String node2) {
+    public Edge(Vertex node1, Vertex node2) {
         this.node1 = node1;
         this.node2 = node2;
     }
