74ad504f4666cc2b63afeefeb312c6849d9ff050
jrundht <joerund.tangen@gmail.com>
2023-10-16 11:43:29 +0200

    Added method to find actor by name

--- Oblig3/GraphBuilder.java
+++ Oblig3/GraphBuilder.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 public class GraphBuilder {
     Graph graph = new Graph();
+    HashMap<String, Actor> actors = new HashMap<>();
 
     public void addMovies(ArrayList<String> lines){
         for(String line : lines){
             String[] parts = line.split("\t");
             Vertex v = graph.addVertex(line);
-            // graph.E.putIfAbsent(v, new HashSet<>()); //in case somehow a movie is twice in the same list
             graph.E.put(v, new HashSet<>()); //in case somehow a movie is twice in the same list
         }
     }
@@ -18,26 +20,31 @@ public class GraphBuilder {
      */
     public void addActors(ArrayList<String> lines){
         for(String line : lines){
-            //first, make vertex
-            Vertex v = graph.addVertex(line);
-            // graph.E.putIfAbsent(v, new HashSet<>()); //in case somehow an actor is twice in the same list
-            graph.E.put(v, new HashSet<>()); //in case somehow an actor is twice in the same list
-
             String[] parts = line.split("\t");
+            
+            Vertex v = graph.addVertex(line);
+            graph.E.put(v, new HashSet<>()); //in case somehow an actor is twice in the same list
+            actors.put(parts[1], (Actor) v);
+            
             Movie u = null;
+            
             //connect to movies
             for(int i = 2; i < parts.length; i++){
                 u = (Movie) graph.findByID(parts[i]);
                 
-                if(u == null) break;
-
+                if(u == null) continue;
                 graph.addEdge(v, u);
-                graph.addEdge(u, v);
+                graph.addEdge(u, v); 
 
                 graph.w.put(new Edge(u, v), u.getRating());
                 graph.w.put(new Edge(v, u), u.getRating());
             }
+
         }
+    }
+
+    public Actor findActor(String name){
+        return actors.get(name);
     }
 
     public void printNodesEdges(){
