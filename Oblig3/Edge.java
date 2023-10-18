23ea6a7af872bcdbff7abdd1e570bfe9e19b0239
jrundht <joerund.tangen@gmail.com>
2023-10-18 17:57:02 +0200

    No longer extends Tuple

--- Oblig3/Edge.java
+++ Oblig3/Edge.java
@@ -1,7 +1,31 @@
 import java.util.*;
 
-public class Edge extends Tuple<Vertex, Vertex>{
-    public Edge(Vertex node1, Vertex node2) {
-        super(node1, node2);
+public class Edge{
+    Vertex element1;
+    Vertex element2;
+
+    public Edge(Vertex element1, Vertex element2) {
+        this.element1 = element1;
+        this.element2 = element2;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        @SuppressWarnings("unchecked")
+        Edge pair = (Edge) obj;
+        return element1.equals(pair.element1) && element2.equals(pair.element2);
+    }
+
+    @Override
+    //Gives an id for iterating in a HashMap
+    public int hashCode() {
+        return Objects.hash(element1, element2);
+    }
+
+    @Override
+    public String toString(){
+        return "(" + element1 + ", " + element2 + ")";
     }
 }
