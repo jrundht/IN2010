c4ccd9677e8a8ea808da0cf2bcf4f9fbc9f77d63
jrundht <joerund.tangen@gmail.com>
2023-10-18 18:00:54 +0200

    -

--- Oblig3/m.java
+++ Oblig3/m.java
@@ -49,7 +49,32 @@ public class m{
             inp = scanner.nextLine();
         }
         scanner.close();
-    }    
+    }
+
+    public static void bfs(Actor actor1, Actor actor2){
+        ArrayList<Vertex> path = ShortestPath.bfsShortestPath(g,actor1, actor2);
+        // System.out.println("\n" + "Shortest path: " + actor1 + " and " + actor2 + " are connected through:");
+        System.out.println("\n" + actor1);
+        for(int i = 1; i < path.size()-1; i ++){
+            System.out.println("===[ " + path.get(i) + " ] ===> " + path.get(++i));
+        }
+    }
+    
+    public static void chillest(Actor actor1, Actor actor2){
+        ArrayList<Vertex> path = ShortestPath.chillestPathBetween(g,actor1, actor2);
+        // System.out.println("\n" + "Most enjoyable path: " + actor1 + " and " + actor2 + " are connected through:");
+        System.out.println("\n" + actor1);
+        double w = 0.0;
+        for(int i = 1; i < path.size()-1; i ++){
+            if(path.get(i) instanceof Movie){
+                Movie m = (Movie) path.get(i);
+                w += 10 - m.getRating();
+            }
+            System.out.println("===[ " + path.get(i) + " ] ===> " + path.get(++i));
+        }
+        System.out.printf("Total weight: %,.1f", w);
+        System.out.println();
+    }
 
     public static void main(String[] args){
         ArrayList<String> movies = FileReadr.readFile("six-degrees-of-imdb-ressursside-main/movies.tsv"); //O(N)
@@ -58,23 +83,38 @@ public class m{
         ArrayList<String> actors = FileReadr.readFile("six-degrees-of-imdb-ressursside-main/actors.tsv"); //O(N)
         graph.addActors(actors);
 
-        lookUp();
+        // Find path between two actors
+        // lookUp();
 
-        // ArrayList<Vertex> path = ShortestPath.shortestPathBetween(g, graph.actors.get("Donald Glover"), graph.actors.get("Jeremy Irons"));
-        // System.out.println("\n" + "Shortest path: Donald Glover" + " and " + "Jeremy Irons" + " are connected through:");
-        // for(int i = 1; i < path.size()-1; i ++){
-        //     System.out.println("===[ " + path.get(i) + " ] ===> " + path.get(++i));
-                    
-        // }
-        // path = ShortestPath.chillestPathBetween(g, graph.actors.get("Donald Glover"), graph.actors.get("Jeremy Irons"));
-        // System.out.println("\n" + "Chillest path: Donald Glover" + " and " + "Jeremy Irons" + " are connected through:");
-        // for(int i = 1; i < path.size()-1; i ++){
-        //     System.out.println("===[ " + path.get(i) + " ] ===> " + path.get(++i));
-                    
-        // }
+        // ShortestPath.bfsShortestPath(g,graph.findActor("Donald Glover"), graph.findActor("Jeremy Irons"));
+        // Shortest path with bfs
+        System.out.println("\nShortest path:");
+        bfs(graph.findActor("Donald Glover"), graph.findActor("Jeremy Irons"));
         
+        bfs(graph.findActor("Scarlett Johansson"), graph.findActor("Emma Mackey"));
+        
+        bfs(graph.findActor("Carrie Coon"), graph.findActor("Julie Delpy"));
+        
+        bfs(graph.findActor("Christian Bale"), graph.findActor("Lupita Nyong'o"));
+       
+        bfs(graph.findActor("Tuva Novotny"), graph.findActor("Michael K. Williams"));
+
+        //Chillest path
+        System.out.println("\nChillest path:");
+        chillest(graph.findActor("Donald Glover"), graph.findActor("Jeremy Irons"));
+
+        chillest(graph.findActor("Scarlett Johansson"), graph.findActor("Emma Mackey"));
+
+        chillest(graph.findActor("Carrie Coon"), graph.findActor("Julie Delpy"));
+        
+        chillest(graph.findActor("Christian Bale"), graph.findActor("Lupita Nyong'o"));
+       
+        chillest(graph.findActor("Tuva Novotny"), graph.findActor("Michael K. Williams"));
+        
+        // Components
         HashMap<Integer, Integer> components = ShortestPath.components(g);
         TreeMap<Integer, Integer> sortedMap = new TreeMap<>(components);
+        System.out.println();
         for(int i : sortedMap.keySet()){
             System.out.println("There are " + sortedMap.get(i) + " components of size " + i);
         }
