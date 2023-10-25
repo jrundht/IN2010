767f4cd984cb29c57ad7f1ed994862cc9d932a93
jrundht <joerund.tangen@gmail.com>
2023-10-25 18:02:23 +0200

    Changed initial size of hashmaps

--- Oblig3/Graph.java
+++ Oblig3/Graph.java
@@ -3,10 +3,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Graph {
-    Set<Vertex> V = new HashSet<>();                    //All Nodes/Vertices
-    HashMap<String, Vertex> vertices = new HashMap<>(); //Hashmap to find vertices by id
-    HashMap<Vertex, Set<Vertex>> E = new HashMap<>();   //All Edges, represented
-    HashMap<Edge, Double> w = new HashMap<>();          //Edges, and weight/the cost to cross from one to the other
+    Set<Vertex> V = new HashSet<>(250000);                    //All Nodes/Vertices
+    HashMap<String, Vertex> vertices = new HashMap<>(250000); //Hashmap to find vertices by id
+    HashMap<Vertex, Set<Vertex>> E = new HashMap<>(250000);   //All Edges, represented
+    HashMap<Edge, Double> w = new HashMap<>(2000000);          //Edges, and weight/the cost to cross from one to the other
 
     public Vertex addVertex(String data){
         Vertex newVertex = null;
